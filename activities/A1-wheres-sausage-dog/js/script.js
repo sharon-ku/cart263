c6dd5c3ba3c9a196c75e4858ae4a0a4717723d5f
sharon-ku <sharon-ku@users.noreply.github.com>
2021-01-15 11:39:45 -0500

    A1: preloaded all animal images; hid scrollbars; created canvas and added bg color; added global description

--- activities/A1-wheres-sausage-dog/js/script.js
+++ activities/A1-wheres-sausage-dog/js/script.js
@@ -1,20 +1,49 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Assignment 1: Where's Sausage Dog
+Sharon Ku
 
-Here is a description of this template p5 project.
+The canvas will display many random images of animals at random positions,
+one of which will be a sausage dog.
+The player has to click on the sausage dog to win the game.
+When clicked successfully, the sausage dog will start spinning.
 **************************************************/
+
+"use strict";
+
+// number of animal images
+const NUM_ANIMAL_IMAGES = 10;
+
+// array that stores animal images
+let animalImages = [];
+
+// background fill color: vibrant green
+let bgFill = {
+  r: 84,
+  g: 227,
+  b: 151,
+};
+
+// preload()
+//
+// Preload all images
+function preload() {
+  // loop through all animal images and push into animalImages array
+  for (let i = 0; i < NUM_ANIMAL_IMAGES; i++) {
+    let animalImage = loadImage(`assets/images/animal${i}.png`);
+    animalImages.push(animalImage);
+  }
+}
 
 // setup()
 //
-// Description of setup() goes here.
+// Create canvas and new objects here
 function setup() {
-
+  createCanvas(windowWidth, windowHeight);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-
+  background(bgFill.r, bgFill.g, bgFill.b);
 }
