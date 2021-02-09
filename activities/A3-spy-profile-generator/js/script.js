8baa3f482ccce2704d68750aa299d26e483ecef0
sharon-ku <sharon-ku@users.noreply.github.com>
2021-02-08 20:48:34 -0500

    A3: added user prompt for spy name; displayed text containing all spy properties on canvas; store spy name

--- activities/A3-spy-profile-generator/js/script.js
+++ activities/A3-spy-profile-generator/js/script.js
@@ -5,10 +5,41 @@ Sharon Ku
 Here is a description of this template p5 project.
 **************************************************/
 
+"use strict";
+
+// stores spy profile information
+let spyProfile = {
+  name: `stranger`,
+  alias: `potato head`,
+  secretWeapon: `water bottle`,
+  password: `kitkat`
+};
+
+// background color
+let bgColor = 0;
+
+
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+  // Create a canvas
+  createCanvas(windowWidth, windowHeight);
+
+  // Try to load the user data
+  let data = JSON.parse(localStorage.getItem(`spy-profile-name`));
+
+  // Check if there's data there
+  if (data) {
+    // If there is, use the name in the data
+    spyProfile.name = data.name;
+  }
+else {
+  // If there's no data, ask the user to enter their spy name
+  spyProfile.name = prompt(`Enter your uber secret spy name`, `Bob`);
+  // Save the user's name
+  localStorage.setItem(`spy-profile-name`, JSON.stringify(spyProfile));
+}
 
 }
 
@@ -16,5 +47,22 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
+  // Set a background color
+  background(bgColor);
 
+  let profile = `**SPY PROFILE**
+
+Name: ${spyProfile.name}
+Alias: ${spyProfile.alias}
+Secret Weapon: ${spyProfile.secretWeapon}
+Password: ${spyProfile.password}`;
+
+  // Display spy name
+  push();
+  fill(255);
+  textFont(`Courier, monospace`)
+  textAlign(LEFT, TOP);
+  textSize(30);
+  text(profile, 100, 100);
+  pop();
 }
