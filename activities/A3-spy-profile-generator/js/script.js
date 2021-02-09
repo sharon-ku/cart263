f6fc647b70c1b8f8b52b9f9a86334f6aa49e1a2f
sharon-ku <sharon-ku@users.noreply.github.com>
2021-02-09 16:39:26 -0500

    A3: save the spy profile in the browser

--- activities/A3-spy-profile-generator/js/script.js
+++ activities/A3-spy-profile-generator/js/script.js
@@ -45,23 +45,20 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 
   // Try to load the user data
-  let data = JSON.parse(localStorage.getItem(`spy-profile-name`));
+  let data = JSON.parse(localStorage.getItem(`spy-profile-data`));
 
-  // Check if there's data there
+  // Check if there's saved data, then copy it into the profile
   if (data) {
     // If there is, use the name in the data
     spyProfile.name = data.name;
+    spyProfile.alias = data.alias;
+    spyProfile.secretWeapon = data.secretWeapon;
+    spyProfile.password = data.password;
   }
   else {
-    // If there's no data, ask the user to enter their spy name
-    spyProfile.name = prompt(`Enter your uber secret spy name`, `Bob`);
-    // Save the user's name
-    localStorage.setItem(`spy-profile-name`, JSON.stringify(spyProfile));
+    // If there's no data, generate a spy profile
+    generateSpyProfile();
   }
-
-  // Generate a spy profile
-  generateSpyProfile();
-
 }
 
 // Generate a spy profile
@@ -69,12 +66,17 @@ function generateSpyProfile() {
   // Find a random tarot card
   let card = random(tarotData.tarot_interpretations);
 
+  // Set the spy's name to what the user types in
+  spyProfile.name = prompt(`Enter your uber secret spy name`);
   // Set the spy's alias to a random strange word
   spyProfile.alias = `The ${random(strangeWordData.words)}`;
   // Set the spy's secret weapon to a random object
   spyProfile.secretWeapon = random(objectData.objects);
   // Set the spy's password to a random key word from the tarot interpretations
   spyProfile.password = random(card.keywords);
+
+  // Save the user's spy profile
+  localStorage.setItem(`spy-profile-data`, JSON.stringify(spyProfile));
 }
 
 // draw()
@@ -84,8 +86,7 @@ function draw() {
   // Set a background color
   background(bgColor);
 
-
-
+  // Store spy profile text
   let profile = `**SPY PROFILE**
 
 Name: ${spyProfile.name}
@@ -93,7 +94,7 @@ Alias: ${spyProfile.alias}
 Secret Weapon: ${spyProfile.secretWeapon}
 Password: ${spyProfile.password}`;
 
-  // Display spy name
+  // Display spy profile
   push();
   fill(255);
   textFont(`Courier, monospace`)
