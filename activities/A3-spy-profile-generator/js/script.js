e60f542ee65de9bd34b78190e4756a86c507e634
sharon-ku <sharon-ku@users.noreply.github.com>
2021-02-08 21:19:38 -0500

    A3: successfully generate a spy profile

--- activities/A3-spy-profile-generator/js/script.js
+++ activities/A3-spy-profile-generator/js/script.js
@@ -18,6 +18,24 @@ let spyProfile = {
 // background color
 let bgColor = 0;
 
+// strange word
+let strangeWordData = undefined;
+
+// common object
+let objectData = undefined;
+
+// tarot interpretation
+let tarotData = undefined;
+
+// Preload JSON files
+function preload() {
+  // Load JSON file containing a list of strange words
+  strangeWordData = loadJSON(`assets/data/strange_words.json`);
+  // Load JSON file containing a list of objects
+  objectData = loadJSON(`assets/data/objects.json`);
+  // Load JSON file containing a list of tarot interpretations
+  tarotData = loadJSON(`assets/data/tarot_interpretations.json`);
+}
 
 // setup()
 //
@@ -34,13 +52,29 @@ function setup() {
     // If there is, use the name in the data
     spyProfile.name = data.name;
   }
-else {
-  // If there's no data, ask the user to enter their spy name
-  spyProfile.name = prompt(`Enter your uber secret spy name`, `Bob`);
-  // Save the user's name
-  localStorage.setItem(`spy-profile-name`, JSON.stringify(spyProfile));
+  else {
+    // If there's no data, ask the user to enter their spy name
+    spyProfile.name = prompt(`Enter your uber secret spy name`, `Bob`);
+    // Save the user's name
+    localStorage.setItem(`spy-profile-name`, JSON.stringify(spyProfile));
+  }
+
+  // Generate a spy profile
+  generateSpyProfile();
+
 }
 
+// Generate a spy profile
+function generateSpyProfile() {
+  // Find a random tarot card
+  let card = random(tarotData.tarot_interpretations);
+
+  // Set the spy's alias to a random strange word
+  spyProfile.alias = `The ${random(strangeWordData.words)}`;
+  // Set the spy's secret weapon to a random object
+  spyProfile.secretWeapon = random(objectData.objects);
+  // Set the spy's password to a random key word from the tarot interpretations
+  spyProfile.password = random(card.keywords);
 }
 
 // draw()
@@ -49,6 +83,8 @@ else {
 function draw() {
   // Set a background color
   background(bgColor);
+
+
 
   let profile = `**SPY PROFILE**
 
