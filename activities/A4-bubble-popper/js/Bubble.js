2230365a7d1098787d6f0dd5b41d41885a26bcca
sharon-ku <sharon-ku@users.noreply.github.com>
2021-02-16 16:23:54 -0500

    A4: displayed a pin that runs along length of index finger; created a bubble that is displayed and floats around; created loading (displays Loading text) and running state

---
+++ activities/A4-bubble-popper/js/Bubble.js
@@ -0,0 +1,60 @@
+// Bubble floating around that is meant to be popped!
+class Bubble {
+  constructor() {
+    // position
+    this.x = random(0, width);
+    this.y = random(0, height);
+    // movement
+    this.vx = 0;
+    this.vy = 0;
+    this.speed = 1;
+    // appearance
+    this.size = random(30, 60);
+    this.fill = {
+      r: random(0, 255),
+      g: random(0, 255),
+      b: random(0, 255),
+      alpha: 125,
+    };
+  }
+
+  // Update all behaviour of bubble
+  update() {
+    // Display bubble
+    this.display();
+    // Move bubble randomly on canvas
+    this.float();
+  }
+
+  // Move bubble randomly on canvas
+  float() {
+    // Change velocity 10% of the time
+    if (random() < 0.1) {
+      this.vx = random(-this.speed, this.speed);
+      this.vy = random(-this.speed, this.speed);
+    }
+
+    // Update position with velocity
+    this.x += this.vx;
+    this.y += this.vy;
+
+  }
+
+  // Display bubble as ellipse
+  display() {
+    push();
+    noStroke();
+    fill(this.fill.r, this.fill.g, this.fill.b, this.fill.alpha);
+    ellipse(this.x, this.y, this.size);
+    pop();
+  }
+
+  // // Returns true if subject overlaps with bubble
+  // overlapsWith(subject) {
+  //   if (subject.) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
+}
