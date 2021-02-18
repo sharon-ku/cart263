d97a74917baf9a945660df3e6c32e242e2a99d5d
sharon-ku <sharon-ku@users.noreply.github.com>
2021-02-18 18:01:15 -0500

    P1: created template json file to fill out vocab words and information; created title rectangle

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -1,14 +1,51 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Project 1: Learn English
+Sharon Ku
 
 Here is a description of this template p5 project.
 **************************************************/
+
+// Vocabulary word
+let vocabularyWord;
+
+// Background color
+let bgFill = {
+  r: 0,
+  g: 0,
+  b: 0
+};
+
+// Title rectangle
+let titleRectangle;
+
+
+// preload()
+//
+// Preload images, json files
+function preload() {
+  vocabularyWord = loadJSON("assets/data/vocabularyWords.json");
+}
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+  // Create canvas, remove strokes
+  createCanvas(1280, 720);
+  noStroke();
+
+  // Create a new rectangle for title
+  let titleRectangleProperties  = {
+    x: width / 2,
+    y: height / 2,
+    width: width - 50,
+    height: height - 50,
+    cornerRadius: 20,
+    fillR: 70,
+    fillG: 197,
+    fillB: 149,
+  };
+  titleRectangle = new Rectangle(titleRectangleProperties);
 
 }
 
@@ -16,5 +53,9 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
+  // Set background color
+  background(bgFill.r, bgFill.g, bgFill.b);
 
+  // Display title rectangle
+  titleRectangle.display();
 }
