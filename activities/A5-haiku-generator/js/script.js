4d26834f77e90670a850d77832dc5db96891bddb
sharon-ku <sharon-ku@users.noreply.github.com>
2021-03-10 08:59:14 -0500

    A5: added fiveSyllableLines and sevenSyllable Lines arrays

--- activities/A5-haiku-generator/js/script.js
+++ activities/A5-haiku-generator/js/script.js
@@ -1,20 +1,31 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Activity 5: Haiku Generator
+Sharon Ku
 
 Here is a description of this template p5 project.
 **************************************************/
 
+let fiveSyllablelines = [
+  `0, to be a tree,`,
+  `The cat does not know,`,
+  `We are all forests,`,
+  `You have done your best,`,
+  `They are all gone now`,
+];
+let sevenSyllanbleLines = [
+  `Say the things left unsaid,`,
+  `Never believe the wind's lies,`,
+  `The autumn stretches its legs,`,
+  `Nothing can satisfy you,`,
+  `They will not come back again`,
+];
+
 // setup()
 //
 // Description of setup() goes here.
-function setup() {
-
-}
+function setup() {}
 
 // draw()
 //
 // Description of draw() goes here.
-function draw() {
-
-}
+function draw() {}
