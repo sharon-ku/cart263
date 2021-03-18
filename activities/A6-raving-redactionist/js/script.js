3a8175dc4c7031c547255aa26da25e11a34cbd5f
sharon-ku <sharon-ku@users.noreply.github.com>
2021-03-18 11:16:19 -0400

    A6: added jQuery library; set up HTML file; added CSS

--- activities/A6-raving-redactionist/js/script.js
+++ activities/A6-raving-redactionist/js/script.js
@@ -1,11 +1,18 @@
 /**
-Title of Project
-Author Name
+Activity 6: Raving Redactionist
+Sharon Ku
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+We will create a webpage of text with some passages “redacted” (covered in black bars).
+Over time the redactions will disappear, revealing the shocking secret text beneath them.
+The user is in charge of keeping the information secret, so they click the secret text to restore the redaction
 */
 
 "use strict";
 
 // Code goes here
+
+/* When user clicks on redacted text, re-redact the text */
+$(`.redacted`).on(`click`, function (event) {
+  $(this).css(`background`, `black`);
+  $(this).css(`user-select`, `none`);
+});
