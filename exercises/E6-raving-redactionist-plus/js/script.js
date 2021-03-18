b7669febab6f1f9a343973397422011bffc029e7
sharon-ku <sharon-ku@users.noreply.github.com>
2021-03-18 11:46:09 -0400

    E6: started exercise 6

---
+++ exercises/E6-raving-redactionist-plus/js/script.js
@@ -0,0 +1,36 @@
+/**
+Activity 6: Raving Redactionist
+Sharon Ku
+
+We will create a webpage of text with some passages “redacted” (covered in black bars).
+Over time the redactions will disappear, revealing the shocking secret text beneath them.
+The user is in charge of keeping the information secret, so they click the secret text to restore the redaction
+*/
+
+"use strict";
+
+// When user clicks on redacted text, re-redact the text
+$(`.top-secret`).on(`click`, function (event) {
+  // Remove the revealed class
+  $(this).removeClass(`revealed`);
+  // Add the redacted class
+  $(this).addClass(`redacted`);
+});
+
+// Remove redacted class over time
+setInterval(revelation, 500);
+
+function revelation() {
+  $(`.redacted`).each(attemptReveal);
+}
+
+// Attempt a hack
+function attemptReveal() {
+  // Hack succeeds 10% of the time
+  if (Math.random() < 0.1) {
+    // Remove the redacted class
+    $(this).removeClass(`redacted`);
+    // Add the revealed class
+    $(this).addClass(`revealed`);
+  }
+}
