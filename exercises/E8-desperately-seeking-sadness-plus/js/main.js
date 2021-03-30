e569cdcbf79012e4d3e04e90727cc938b5bbcac3
sharon-ku <sharon-ku@users.noreply.github.com>
2021-03-30 17:46:07 -0400

    E8: added sound sources

--- exercises/E8-desperately-seeking-sadness-plus/js/main.js
+++ exercises/E8-desperately-seeking-sadness-plus/js/main.js
@@ -6,6 +6,15 @@ We will create metaphor for how we seek out or are drawn to negative emotions.
 This will be in the form of a spaceship in a kind of asteroid field! Obvi!
 The user will fly around as an emoji, seeking out the single “thumbs down” in a sea of “thumbs ups”.
 When they find it, another one will appear somewhere else and the sad saga continues.
+
+SOUND SOURCES ----
+Background music:
+Unexpected Distress by onlymeith (c) copyright 2020 Licensed under a Creative Commons Attribution Noncommercial  (3.0) license. http://dig.ccmixter.org/files/onlymeith/61318 Ft: Carosone
+
+Arcade sound effect:
+Arcade retro game over
+https://mixkit.co/free-sound-effects/
+
 */
 
 "use strict";
