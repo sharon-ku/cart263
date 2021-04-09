fe8d25dd72754bee7b603bb9f3faefdb9dc45494
sharon-ku <sharon-ku@users.noreply.github.com>
2021-04-09 14:53:26 -0400

    P2: moved all canvas creation into functions

--- projects/project2/js/instance1Start.js
+++ projects/project2/js/instance1Start.js
@@ -1,101 +1,103 @@
 // Instance 1: Start canvas
 //
-let instance1Sketch = function (p) {
-  // Mouse position
-  let mouse = {
-    x: undefined,
-    y: undefined,
+function createTitleCanvas() {
+  let instance1Sketch = function (p) {
+    // Mouse position
+    let mouse = {
+      x: undefined,
+      y: undefined,
+    };
+
+    // Drop
+    let drop = undefined;
+    // Drop image
+    let dropImage = undefined;
+
+    // Start rectangle that spans width of window
+    let startRectangle = undefined;
+
+    // Pulsating circle
+    let pulsatingCircle = undefined;
+
+    // Background color: black
+    let bgFill = {
+      r: 0,
+      g: 0,
+      b: 0,
+    };
+
+    // Load assets
+    p.preload = function () {
+      // Load drop image
+      dropImage = p.loadImage(`assets/images/drop.png`);
+    };
+
+    // Create canvas and objects
+    p.setup = function () {
+      // Create a start canvas
+      let startCanvas = p.createCanvas(p.windowWidth, p.windowHeight);
+      startCanvas.parent(`start-canvas`);
+
+      // Create new start rectangle whose position is at the center of the canvas
+      let rectangleX = p.width / 2;
+      let rectangleY = p.height / 2;
+      let rectangleWidth = p.width;
+      startRectangle = new StartRectangle(
+        p,
+        rectangleX,
+        rectangleY,
+        rectangleWidth
+      );
+
+      // Create a new pulsating circle
+      let circleX = p.width / 2;
+      let circleY = p.height / 2;
+      pulsatingCircle = new PulsatingCircle(p, circleX, circleY);
+
+      // Create a new drop
+      let dropX = p.width / 2;
+      let dropY = -10;
+      drop = new Drop(p, dropImage, dropX, dropY);
+    };
+
+    // Set mouse positions, set background color, update behaviour of rectangle, pulsating circle, and drop
+    p.draw = function () {
+      // Set mouse x and y positions
+      mouse.x = p.mouseX;
+      mouse.y = p.mouseY;
+
+      // Set background color
+      p.background(bgFill.r, bgFill.g, bgFill.b);
+
+      // Update all behaviour of rectangle
+      startRectangle.update();
+
+      // Update circle behaviour
+      pulsatingCircle.update();
+
+      // If mouse clicked on circle, release drop
+      if (pulsatingCircle.overlapsWith(mouse) && p.mouseIsPressed) {
+        drop.release = true;
+      }
+
+      // If time to release drop, update drop behaviour
+      if (drop.release) {
+        // Update all behaviour of drop
+        drop.update(pulsatingCircle);
+      }
+
+      // If drop overlaps with circle, make circle expand throughout entire canvas
+      if (pulsatingCircle.overlapsWith(drop)) {
+        pulsatingCircle.expandAllTheWay();
+      }
+
+      // If circle exceeds rectangle height, display dialog boxes
+      if (pulsatingCircle.size.current > startRectangle.height && state === `title`) {
+        welcome();
+      }
+    };
+
   };
 
-  // Drop
-  let drop = undefined;
-  // Drop image
-  let dropImage = undefined;
-
-  // Start rectangle that spans width of window
-  let startRectangle = undefined;
-
-  // Pulsating circle
-  let pulsatingCircle = undefined;
-
-  // Background color: black
-  let bgFill = {
-    r: 0,
-    g: 0,
-    b: 0,
-  };
-
-  // Load assets
-  p.preload = function () {
-    // Load drop image
-    dropImage = p.loadImage(`assets/images/drop.png`);
-  };
-
-  // Create canvas and objects
-  p.setup = function () {
-    // Create a start canvas
-    let startCanvas = p.createCanvas(p.windowWidth, p.windowHeight);
-    startCanvas.parent(`start-canvas`);
-
-    // Create new start rectangle whose position is at the center of the canvas
-    let rectangleX = p.width / 2;
-    let rectangleY = p.height / 2;
-    let rectangleWidth = p.width;
-    startRectangle = new StartRectangle(
-      p,
-      rectangleX,
-      rectangleY,
-      rectangleWidth
-    );
-
-    // Create a new pulsating circle
-    let circleX = p.width / 2;
-    let circleY = p.height / 2;
-    pulsatingCircle = new PulsatingCircle(p, circleX, circleY);
-
-    // Create a new drop
-    let dropX = p.width / 2;
-    let dropY = -10;
-    drop = new Drop(p, dropImage, dropX, dropY);
-  };
-
-  // Set mouse positions, set background color, update behaviour of rectangle, pulsating circle, and drop
-  p.draw = function () {
-    // Set mouse x and y positions
-    mouse.x = p.mouseX;
-    mouse.y = p.mouseY;
-
-    // Set background color
-    p.background(bgFill.r, bgFill.g, bgFill.b);
-
-    // Update all behaviour of rectangle
-    startRectangle.update();
-
-    // Update circle behaviour
-    pulsatingCircle.update();
-
-    // If mouse clicked on circle, release drop
-    if (pulsatingCircle.overlapsWith(mouse) && p.mouseIsPressed) {
-      drop.release = true;
-    }
-
-    // If time to release drop, update drop behaviour
-    if (drop.release) {
-      // Update all behaviour of drop
-      drop.update(pulsatingCircle);
-    }
-
-    // If drop overlaps with circle, make circle expand throughout entire canvas
-    if (pulsatingCircle.overlapsWith(drop)) {
-      pulsatingCircle.expandAllTheWay();
-    }
-
-    // If circle exceeds rectangle height, display dialog boxes
-    if (pulsatingCircle.size.current > startRectangle.height && state === `title`) {
-      welcome();
-    }
-  };
-
-};
-
-// let myp5Start = new p5(instance1Sketch);
+  let myp5Start = new p5(instance1Sketch);
+}
--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -21,287 +21,316 @@ const NUM_TOTAL_PUZZLES = 2;
 // -------------------------------------------------------------------
 
 if (state === `title`) {
-  let myp5Start = new p5(instance1Sketch);
+  createTitleCanvas();
 }
 
-
-
 // Create welcome canvas
-let sketch = function (p) {
-  // Rectangle properties
-  let rect = {
-    // position
-    x: undefined,
-    y: undefined,
-    // color
-    fill: 255,
-    // size + growth
-    size: {
-      current: 0,
-      min: 0,
-      max: 300,
-      growthRate: {
-        initial: 0.005,
-        current: 0.005,
+function createWelcomeCanvas() {
+  let sketch = function (p) {
+    // Rectangle properties
+    let rect = {
+      // position
+      x: undefined,
+      y: undefined,
+      // color
+      fill: 255,
+      // size + growth
+      size: {
+        current: 0,
+        min: 0,
+        max: 300,
+        growthRate: {
+          initial: 0.005,
+          current: 0.005,
+        },
+        growthAcceleration: 0.01,
       },
-      growthAcceleration: 0.01,
-    },
-  };
+    };
 
-  // Background color: light purple
-  let bgFill = {
-    r: 240,
-    g: 170,
-    b: 255,
-  };
+    // Background color: light purple
+    let bgFill = {
+      r: 240,
+      g: 170,
+      b: 255,
+    };
 
-  // Create canvas and set rectangle position
-  p.setup = function () {
-    // Create a welcome canvas
-    let welcomeCanvas = p.createCanvas(300, 300);
-    welcomeCanvas.parent(`welcome-canvas`);
+    // Create canvas and set rectangle position
+    p.setup = function () {
+      // Create a welcome canvas
+      let welcomeCanvas = p.createCanvas(300, 300);
+      welcomeCanvas.parent(`welcome-canvas`);
 
-    // Set rectangle position to center of canvas
-    rect.x = p.width / 2;
-    rect.y = p.height / 2;
-  };
+      // Set rectangle position to center of canvas
+      rect.x = p.width / 2;
+      rect.y = p.height / 2;
+    };
 
-  // Set background color, draw and make rectangle grow
-  p.draw = function () {
-    // Set background color
-    p.background(bgFill.r, bgFill.g, bgFill.b);
+    // Set background color, draw and make rectangle grow
+    p.draw = function () {
+      // Set background color
+      p.background(bgFill.r, bgFill.g, bgFill.b);
+
+      // Draw rectangle
+      p.drawRectangle();
+
+      // Make rectangle grow
+      p.rectangleGrows();
+    };
 
     // Draw rectangle
-    p.drawRectangle();
+    p.drawRectangle = function () {
+      p.push();
+      p.rectMode(p.CENTER);
+      p.fill(rect.fill);
+      p.rect(rect.x, rect.y, rect.size.current);
+      p.pop();
+    };
 
     // Make rectangle grow
-    p.rectangleGrows();
+    p.rectangleGrows = function () {
+      // If rectangle has not reached max size yet
+      if (rect.size.current < rect.size.max) {
+        // Make rectangle grow on infinitely with speed and acceleration
+        rect.size.growthRate.current += rect.size.growthAcceleration;
+        rect.size.current += rect.size.growthRate.current;
+      }
+      // Else, if rectangle exceeds max size
+      else {
+        // Reset size
+        rect.size.current = rect.size.min;
+        // Reset growth rate
+        rect.size.growthRate.current = rect.size.growthRate.initial;
+      }
+    };
   };
 
-  // Draw rectangle
-  p.drawRectangle = function () {
-    p.push();
-    p.rectMode(p.CENTER);
-    p.fill(rect.fill);
-    p.rect(rect.x, rect.y, rect.size.current);
-    p.pop();
-  };
-
-  // Make rectangle grow
-  p.rectangleGrows = function () {
-    // If rectangle has not reached max size yet
-    if (rect.size.current < rect.size.max) {
-      // Make rectangle grow on infinitely with speed and acceleration
-      rect.size.growthRate.current += rect.size.growthAcceleration;
-      rect.size.current += rect.size.growthRate.current;
-    }
-    // Else, if rectangle exceeds max size
-    else {
-      // Reset size
-      rect.size.current = rect.size.min;
-      // Reset growth rate
-      rect.size.growthRate.current = rect.size.growthRate.initial;
-    }
-  };
-};
-
-let myp5 = new p5(sketch);
+  let myp5 = new p5(sketch);
+}
 
 // -------------------------------------------------------------------
 // Create distraction canvas
-let sketch2 = function (p) {
-  // Background color: light purple
-  let bgFill = {
-    r: 240,
-    g: 170,
-    b: 255,
+function createDistractionCanvas() {
+  let sketch2 = function (p) {
+    // Background color: light purple
+    let bgFill = {
+      r: 240,
+      g: 170,
+      b: 255,
+    };
+
+    // For video capture
+    let capture;
+    // Properties for video capture display
+    let captureProperties = {
+      // position
+      x: 0,
+      y: 0,
+    };
+
+    // Create canvas and show video of user
+    p.setup = function () {
+      // Create canvas
+      let distractionCanvas = p.createCanvas(320, 240);
+      distractionCanvas.parent(`distraction-canvas`);
+
+      // Show video of user
+      p.createVideoCapture();
+    };
+
+    // Create video of user
+    p.createVideoCapture = function () {
+      capture = p.createCapture(p.VIDEO);
+      capture.size(p.width, p.height);
+      capture.hide();
+    };
+
+    // Set background color and display video capture
+    p.draw = function () {
+      // Set background color
+      p.background(bgFill.r, bgFill.g, bgFill.b);
+
+      // Show video capture
+      p.image(
+        capture,
+        captureProperties.x,
+        captureProperties.y,
+        p.width,
+        p.height
+      );
+    };
   };
 
-  // For video capture
-  let capture;
-  // Properties for video capture display
-  let captureProperties = {
-    // position
-    x: 0,
-    y: 0,
-  };
-
-  // Create canvas and show video of user
-  p.setup = function () {
-    // Create canvas
-    let distractionCanvas = p.createCanvas(320, 240);
-    distractionCanvas.parent(`distraction-canvas`);
-
-    // Show video of user
-    p.createVideoCapture();
-  };
-
-  // Create video of user
-  p.createVideoCapture = function () {
-    capture = p.createCapture(p.VIDEO);
-    capture.size(p.width, p.height);
-    capture.hide();
-  };
-
-  // Set background color and display video capture
-  p.draw = function () {
-    // Set background color
-    p.background(bgFill.r, bgFill.g, bgFill.b);
-
-    // Show video capture
-    p.image(
-      capture,
-      captureProperties.x,
-      captureProperties.y,
-      p.width,
-      p.height
-    );
-  };
-};
-
-let myp52 = new p5(sketch2);
+  let myp52 = new p5(sketch2);
+}
 
 // -------------------------------------------------------------------
 // Create left-puzzle canvas
-let sketch3 = function (p) {
-  // Background color: light blue
-  let bgFill = {
-    r: 121,
-    g: 216,
-    b: 237,
-  };
+function createLeftPuzzleCanvas() {
+  let sketch3 = function (p) {
+    // Background color: light blue
+    let bgFill = {
+      r: 121,
+      g: 216,
+      b: 237,
+    };
 
-  // Eye
-  let eye = {
-    fill: 0,
-    x: 35,
-    y: 40,
-    size: 20,
-  };
+    // Eye
+    let eye = {
+      fill: 0,
+      x: 35,
+      y: 40,
+      size: 20,
+    };
 
-  // Mouth
-  let mouth = {
-    fill: 0,
-    x: 0,
-    y: 75,
-    width: 50,
-    height: 50,
-    startAngle: 0,
-    endAngle: p.PI,
-  };
+    // Mouth
+    let mouth = {
+      fill: 0,
+      x: 0,
+      y: 75,
+      width: 50,
+      height: 50,
+      startAngle: 0,
+      endAngle: p.PI,
+    };
 
-  // Create canvas
-  p.setup = function () {
     // Create canvas
-    let leftPuzzleCanvas = p.createCanvas(70, 120);
-    leftPuzzleCanvas.parent(`left-puzzle-canvas`);
-  };
+    p.setup = function () {
+      // Create canvas
+      let leftPuzzleCanvas = p.createCanvas(70, 120);
+      leftPuzzleCanvas.parent(`left-puzzle-canvas`);
+    };
 
-  // Set background color and display video capture
-  p.draw = function () {
-    // Set background color
-    p.background(bgFill.r, bgFill.g, bgFill.b);
+    // Set background color and display video capture
+    p.draw = function () {
+      // Set background color
+      p.background(bgFill.r, bgFill.g, bgFill.b);
+
+      // Draw face
+      p.drawFace();
+    };
 
     // Draw face
-    p.drawFace();
+    p.drawFace = function () {
+      // Draw mouth
+      p.push();
+      p.fill(mouth.fill);
+      p.arc(
+        mouth.x,
+        mouth.y,
+        mouth.width,
+        mouth.height,
+        mouth.startAngle,
+        mouth.endAngle
+      );
+      p.pop();
+
+      // Draw eye
+      p.push();
+      p.fill(eye.fill);
+      p.ellipse(eye.x, eye.y, eye.size);
+      p.pop();
+    };
   };
 
-  // Draw face
-  p.drawFace = function () {
-    // Draw mouth
-    p.push();
-    p.fill(mouth.fill);
-    p.arc(
-      mouth.x,
-      mouth.y,
-      mouth.width,
-      mouth.height,
-      mouth.startAngle,
-      mouth.endAngle
-    );
-    p.pop();
-
-    // Draw eye
-    p.push();
-    p.fill(eye.fill);
-    p.ellipse(eye.x, eye.y, eye.size);
-    p.pop();
-  };
-};
-
-// let myp53 = new p5(sketch3);
+  let myp53 = new p5(sketch3);
+}
 
 // -------------------------------------------------------------------
 // Create right-puzzle canvas
-let sketch4 = function (p) {
-  // Background color: light blue
-  let bgFill = {
-    r: 121,
-    g: 216,
-    b: 237,
-  };
+function createRightPuzzleCanvas() {
+  let sketch4 = function (p) {
+    // Background color: light blue
+    let bgFill = {
+      r: 121,
+      g: 216,
+      b: 237,
+    };
 
-  // Eye
-  let eye = {
-    fill: 0,
-    x: 35,
-    y: 40,
-    size: 20,
-  };
+    // Eye
+    let eye = {
+      fill: 0,
+      x: 35,
+      y: 40,
+      size: 20,
+    };
 
-  // Mouth
-  let mouth = {
-    fill: 0,
-    x: 70,
-    y: 75,
-    width: 50,
-    height: 50,
-    startAngle: p.PI / 2,
-    endAngle: p.PI,
-  };
+    // Mouth
+    let mouth = {
+      fill: 0,
+      x: 70,
+      y: 75,
+      width: 50,
+      height: 50,
+      startAngle: p.PI / 2,
+      endAngle: p.PI,
+    };
 
-  // Create canvas
-  p.setup = function () {
     // Create canvas
-    let rightPuzzleCanvas = p.createCanvas(70, 120);
-    rightPuzzleCanvas.parent(`right-puzzle-canvas`);
-  };
+    p.setup = function () {
+      // Create canvas
+      let rightPuzzleCanvas = p.createCanvas(70, 120);
+      rightPuzzleCanvas.parent(`right-puzzle-canvas`);
+    };
 
-  // Set background color and display video capture
-  p.draw = function () {
-    // Set background color
-    p.background(bgFill.r, bgFill.g, bgFill.b);
+    // Set background color and display video capture
+    p.draw = function () {
+      // Set background color
+      p.background(bgFill.r, bgFill.g, bgFill.b);
+
+      // Draw face
+      p.drawFace();
+    };
 
     // Draw face
-    p.drawFace();
+    p.drawFace = function () {
+      // Draw mouth
+      p.push();
+      p.fill(mouth.fill);
+      p.arc(
+        mouth.x,
+        mouth.y,
+        mouth.width,
+        mouth.height,
+        mouth.startAngle,
+        mouth.endAngle
+      );
+      p.pop();
+
+      // Draw eye
+      p.push();
+      p.fill(eye.fill);
+      p.ellipse(eye.x, eye.y, eye.size);
+      p.pop();
+    };
   };
 
-  // Draw face
-  p.drawFace = function () {
-    // Draw mouth
-    p.push();
-    p.fill(mouth.fill);
-    p.arc(
-      mouth.x,
-      mouth.y,
-      mouth.width,
-      mouth.height,
-      mouth.startAngle,
-      mouth.endAngle
-    );
-    p.pop();
+  let myp54 = new p5(sketch4);
+}
 
-    // Draw eye
-    p.push();
-    p.fill(eye.fill);
-    p.ellipse(eye.x, eye.y, eye.size);
-    p.pop();
-  };
-};
+// STATE: welcome
+// 
+function welcome() {
+  state = `welcome`;
 
+  // Hide start canvas
+  $(`#start-canvas`).slideToggle();
 
-// let myp54 = new p5(sketch4);
+  // Create canvases
+  createDistractionCanvas();
+  createWelcomeCanvas();
 
+  // Create all dialogs
+  createDistractionDialog();
+  createWelcomeDialog();
+  createCongratulationsDialog();
+
+  // Create left and right puzzle pieces
+  createLeftPuzzleCanvas();
+  createRightPuzzleCanvas();
+
+  // Create puzzle box
+  createPuzzleBox();
+}
 
 // PUZZLE PIECES ----------------------------------------------------
 // Make left puzzle draggable
@@ -341,25 +370,6 @@ function createPuzzleBox() {
       }
     },
   });
-}
-
-function welcome() {
-  state = `welcome`;
-
-  // Hide start canvas
-  $(`#start-canvas`).slideToggle();
-
-  // Create all dialogs
-  createDistractionDialog();
-  createWelcomeDialog();
-  createCongratulationsDialog();
-
-  // Create left and right puzzle pieces
-  let myp53 = new p5(sketch3);
-  let myp54 = new p5(sketch4);
-
-  // Create puzzle box
-  createPuzzleBox();
 }
 
 // CREATE ALL DIALOG BOXES ----------------------------------------------------
